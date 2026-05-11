e91ba352fea154ebaa17aba1a2781518e9510174
chengbinzhang-stack <chengbin.zhang@gmail.com>
2026-05-11 16:02:01 +0800

    jwt

--- food-ordering-backend/src/middleware/auth.ts
+++ food-ordering-backend/src/middleware/auth.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import { jwtDecode } from "jwt-decode";
 
 declare global {
   namespace Express {
@@ -24,8 +25,9 @@ const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
-    req.userId = (decoded as JwtPayload).userId;
+    const decoded = jwtDecode(token); 
+    //jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+    req.userId = String(decoded.sub);
     next();
   } catch {
     return res.status(401).json({ message: "unauthorized" });
